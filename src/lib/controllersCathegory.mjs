e0355d3ffea79167bac784c28e610da3695881a1
marta <ffanecaa@gmail.com>
2023-04-20 10:11:18 +0200

    m

--- src/lib/controllersCathegory.mjs
+++ src/lib/controllersCathegory.mjs
@@ -5,7 +5,7 @@ import { Cathegory } from "./db.mjs";
 async function controllerCathegoryPost(request,response){
     try{
         const cathegory = await Cathegory(request.body)
-        response.setHeader("Contant-Type","application/json")
+        response.setHeader("Content-Type","application/json")
         response.status(201)
         response.send(cathegory.toJSON())
     }catch(error){
