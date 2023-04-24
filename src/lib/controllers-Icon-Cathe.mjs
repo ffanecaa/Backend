ad70da0303e94f4f1c27d9b0a552b5124adb8ec8
marta <ffanecaa@gmail.com>
2023-04-24 19:19:20 +0200

    m

---
+++ src/lib/controllers-Icon-Cathe.mjs
@@ -0,0 +1,24 @@
+import {Icon,Cathegory} from "./db.mjs"
+
+
+
+async function controllerIconCathe(request,response){
+
+try{   const icon= await   Icon.findAll({ atributtes:['id'],
+                include:{model:'Cathegory',
+                          atributtes:['name']}
+})
+response.setHeader("Content-Type","application/json")
+response.status(200)
+response.send(JSON.stringify(icon))
+}catch (error){
+   response.status(500)
+   response.send('falloget')
+}
+
+}
+
+
+export{
+    controllerIconCathe
+}
--- src/main.mjs
+++ src/main.mjs
@@ -3,6 +3,8 @@ import cors from "cors"
 import {controllerElementGet,controllerElementPost,controllerElementDelete,controllerElementPut} from "./lib/controllersElemento.mjs"
 import {controllerIconPost,controllerIconGet} from "./lib/controllersIcon.mjs";
 import {controllerCathegoryPost,controllerCathegoryGet,controllerCathegoryDelete} from "./lib/controllersCathegory.mjs"
+import {   controllerIconCathe} from "./lib/controllers-Icon-Cathe.mjs"
+
 
 
 
@@ -16,6 +18,8 @@ app.delete("/elements/",controllerElementDelete)
 app.put("/elements/",controllerElementPut)
 app.post("/icons/",controllerIconPost)
 app.get("/icons/",controllerIconGet)
+app.get("/icons/",controllerIconCathe)
+
 app.post("/cathegory/",controllerCathegoryPost)
 app.get("/cathegory/",controllerCathegoryGet)
 app.delete("/cathegory/",controllerCathegoryDelete)
