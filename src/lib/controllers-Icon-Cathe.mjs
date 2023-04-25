22e73f96bd07423021cc18a5f343c8c107ed2948
marta <ffanecaa@gmail.com>
2023-04-25 10:52:49 +0200

    Work in progress

--- src/lib/controllers-Icon-Cathe.mjs
+++ src/lib/controllers-Icon-Cathe.mjs
@@ -2,19 +2,22 @@ import {Icon,Cathegory} from "./db.mjs"
 
 
 
-async function controllerIconCathe(request,response){
-
-try{   const icon= await   Icon.findAll({ atributtes:['id'],
-                include:{model:'Cathegory',
-                          atributtes:['name']}
-})
-response.setHeader("Content-Type","application/json")
-response.status(200)
-response.send(JSON.stringify(icon))
-}catch (error){
-   response.status(500)
-   response.send('falloget')
-}
+async function controllerIconCathe(request, response) {
+    try {
+        const icon = await Icon.findAll({
+            atributtes: ['id'],
+            include: {
+                model: 'Cathegory',
+                atributtes: ['name']
+            }
+        })
+        response.setHeader("Content-Type", "application/json")
+        response.status(200)
+        response.send(JSON.stringify(icon))
+    } catch (error) {
+        response.status(500)
+        response.send('falloget')
+    }
 
 }
 
