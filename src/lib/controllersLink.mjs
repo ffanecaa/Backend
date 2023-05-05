e52edfd489b821cf26ba6d69e5f99c85a88a3bda
marta <ffanecaa@gmail.com>
2023-05-05 12:35:37 +0200

    m

--- src/lib/controllersLink.mjs
+++ src/lib/controllersLink.mjs
@@ -1,42 +1,30 @@
 import { Element } from "./db.mjs";
+import {Op} from "sequelize"
 
-
-async function controllerlink(request,response){
-    try{
-       
-        const elemento= await Element.findByPk(
-            request.params.id
-        )
-        response.setHeader("Content-Type","application/json")
-        response.status(200)
-        response.send(JSON.stringify(elemento))
-   
-       }catch (error){
-           response.status(500)
-           response.send('falloget')
-       }
-    }
-async function controllerlinkName(request,response){
-    try{
-       
-        const elemento= await Element.findOne({
-        
-            where:{
-                [Op.substring]:
-             {"name": request.params.name}
-            }
-    
-             } )
-        response.setHeader("Content-Type","application/json")
-        response.status(200)
-        response.send(JSON.stringify(elemento))
-   
-       }catch (error){
-           response.status(500)
-           response.send('falloget')
-       }
-    }
-export {
-    controllerlink,
-    controllerlinkName
+async function controllerlink(request, response) {
+  try {
+    const elemento = await Element.findByPk(request.params.id);
+    response.setHeader("Content-Type", "application/json");
+    response.status(200);
+    response.send(JSON.stringify(elemento));
+  } catch (error) {
+    response.status(500);
+    response.send("falloget");
+  }
 }
+async function controllerlinkName(request, response) {
+  try {
+    const elemento = await Element.findOne({
+      where: {
+        name: { [Op.substring]: request.params.name },
+      },
+    });
+    response.setHeader("Content-Type", "application/json");
+    response.status(200);
+    response.send(JSON.stringify(elemento));
+  } catch (error) {
+    response.status(500);
+    response.send("falloget");
+  }
+}
+export { controllerlink, controllerlinkName };
