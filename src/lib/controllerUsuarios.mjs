dbb00665ba1548c700b5f9bcaac9c8d74b2aeb8a
marta <ffanecaa@gmail.com>
2023-05-25 12:54:33 +0200

    n

--- src/lib/controllerUsuarios.mjs
+++ src/lib/controllerUsuarios.mjs
@@ -13,9 +13,22 @@ async function controllerUsuarioPost(request,response){
  }
 }
 
+//----------
 
-
-
+async function controllerSesion(request,response){
+   try{
+      const usuarios = await Usuarios.findOne({
+         
+      })
+      response.setHeader("Content-Type","application/json")
+      response.status(201)
+      response.json(usuarios.toJSON())
+   } catch(error){
+      response.status(500)
+      response.send(fallo)
+   }
+  }
 export{
-    controllerUsuarioPost
+    controllerUsuarioPost,
+    controllerSesion
 }
